1cab175bf05d954cb1a39de18a301bb35d596b83
Ross DeVito <ross@Rosss-MacBook-Pro.local>
2023-01-30 18:07:22 -0800

    bgen to mt ukb updates (script)

--- scripts/ukb/ukb_19_bgen_to_mt.sh
+++ scripts/ukb/ukb_19_bgen_to_mt.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 bgen-to-mt \
-	-b /mnt/project/Bulk/Exome sequences/Population level exome OQFE variants, BGEN format - final release/ \
-	-w ../../dev_data/proj_data/geno.mt \
+	-b '/mnt/project/Bulk/Exome sequences/Population level exome OQFE variants, BGEN format - final release/ukb23159_c19_b0_v1.bgen' \
+	-w chr19_geno.mt \
 	-i \
-	-s \
-	-e ../../dev_data/proj_data/excluded_samples.tsv
+	-u rdevito_p1_db
