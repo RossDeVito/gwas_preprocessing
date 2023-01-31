50f7e8867979560e137cc60b7daa0f0459683c20
Ross DeVito <ross@Rosss-MacBook-Pro.local>
2023-01-31 15:32:57 -0800

    use sample file in default ukb script

--- scripts/ukb/ukb_19_bgen_to_mt.sh
+++ scripts/ukb/ukb_19_bgen_to_mt.sh
@@ -4,4 +4,5 @@ bgen-to-mt \
 	-b '/mnt/project/Bulk/Exome sequences/Population level exome OQFE variants, BGEN format - final release/ukb23159_c19_b0_v1.bgen' \
 	-w chr19_geno.mt \
 	-i \
+	-s \
 	-u rdevito_p1_db
