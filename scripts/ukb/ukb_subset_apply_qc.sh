8366b8792a9b4744bff97705ddca27f97f3bbdd0
Ross DeVito <ross@Rosss-MBP.lan>
2023-02-02 20:04:06 -0800

    update script to use dx upload

--- scripts/ukb/ukb_subset_apply_qc.sh
+++ scripts/ukb/ukb_subset_apply_qc.sh
@@ -4,14 +4,20 @@ apply-geno-qc \
 	-m split_subset_geno.mt \
 	-v split_subset_variant_qc.ht \
 	-w subset_common_qced.mt \
-	-l /mnt/project/rdevito/project1_data/subset_common_loci.tsv \
+	-l /home/dnanexus/subset_common_loci.tsv \
 	-j ../../scripts/qc_params/dev_common.json \
 	-u rdevito_p1_db
+
+dx upload /home/dnanexus/subset_common_loci.tsv \
+	--path /mnt/project/rdevito/project1_data/
 
 apply-geno-qc \
 	-m split_subset_geno.mt \
 	-v split_subset_variant_qc.ht \
 	-w subset_rare_qced.mt \
-	-l /mnt/project/rdevito/project1_data/subset_rare_loci.tsv \
+	-l /home/dnanexus/subset_rare_loci.tsv \
 	-j ../../scripts/qc_params/dev_rare.json \
 	-u rdevito_p1_db
+
+dx upload /home/dnanexus/subset_rare_loci.tsv \
+	--path /mnt/project/rdevito/project1_data/
