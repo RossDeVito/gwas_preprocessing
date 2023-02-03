663b31106c9b5c2df822989357d34f171e07891e
Ross DeVito <ross@Rosss-MBP.lan>
2023-02-02 17:02:50 -0800

    update apply geno_qc

--- gwas_qc/geno_qc/apply_geno_qc.py
+++ gwas_qc/geno_qc/apply_geno_qc.py
@@ -12,7 +12,7 @@ import hail as hl
 
 """
 run apply_geno_qc.py -m split_subset_geno.mt -v split_subset_variant_qc.ht \
-	-w split_subset_qced.mt -l /mnt/project/rdevito/project1_data/split_subset_qced_loc.bgen \
+	-w split_subset_qced.mt -l /home/dnanexus/locs.tsv \
 	-j ../../scripts/qc_params/dev.json -u rdevito_p1_db
 """
 
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 		variant_qc_path (str, required): Path to saved variant QC
 			Hail Table.
 		write_path_gc_geno (str, required): Path to write QC'd data as MT.
-		write_path_gc_locus_bgen (str): Path to write BGEN with
+		write_path_gc_locus_tsv (str): Path to write TSV with
 			locus/alleles.
 		qc_params_json (str, required): Path to JSON file with QC
 			parameters.
@@ -66,10 +66,10 @@ if __name__ == '__main__':
 	)
 	parser.add_argument(
 		'-l',
-		'--write_path_qc_locus_bgen',
+		'--write_path_gc_locus_tsv',
 		type=str,
 		default=None,
-		help='Path to write BGEN with locus/alleles.',
+		help='Path to write TSV with locus/alleles.',
 	)
 	parser.add_argument(
 		'-j',
@@ -97,7 +97,7 @@ if __name__ == '__main__':
 			'as path to saved variant QC Hail Table, and '
 			'dnax://{ukb_db_name\'s id}/{write_path_qc_geno} as path to'
 			'write QC\'d data as MT. If write_path_qc_locus_vcf is not'
-			'None, will also use file://{write_path_qc_locus_bgen} as'
+			'None, will also use file://{write_path_gc_locus_tsv} as'
 			'path to write VCF with locus/alleles.',
 	)
 	args = parser.parse_args()
@@ -146,51 +146,198 @@ if __name__ == '__main__':
 	variant_qc = hl.read_table(variant_qc_path)
 
 	# Apply variant QC filters to variant_QC
-	# var_mask = None
+	"""
+	'variant_qc': struct {
+        dp_stats: struct {
+            mean: float64, 
+            stdev: float64, 
+            min: float64, 
+            max: float64
+        }, 
+        gq_stats: struct {
+            mean: float64, 
+            stdev: float64, 
+            min: float64, 
+            max: float64
+        }, 
+        AC: array<int32>, 
+        AF: array<float64>, 
+        AN: int32, 
+        homozygote_count: array<int32>, 
+        call_rate: float64, 
+        n_called: int64, 
+        n_not_called: int64, 
+        n_filtered: int64, 
+        n_het: int64, 
+        n_non_ref: int64, 
+        het_freq_hwe: float64, 
+        p_value_hwe: float64, 
+        p_value_excess_het: float64
+    }
 
-	# for qc_key, qc_val in qc_params['variant'].items():
-	# 	if 'dp_stats' in qc_key:
-	# 		if 'min' in qc_key:
-	# 			var_mask = (
-	# 				(variant_qc[qc_key] >= qc_val) &
-	# 				var_mask
-	# 			)
+	Example QC parameters:
+	{
+		"variant": {
+			"call_rate": [0.99, ">="],
+			"p_value_hwe": [1e-6, ">"]
+		},
+		"sample": {
+			"call_rate": [0.99, ">="],
+		}
+	}
+	"""
+	var_filters = []
 
-	variant_qc = variant_qc.filter(
-		(variant_qc.variant_qc.call_rate >= qc_params['variant']['call_rate']) &
-		(variant_qc.variant_qc.p_value_hwe >= qc_params['variant']['p_value_hwe'])
-	)
+	for qc_key, (qc_val, qc_comparison) in qc_params['variant'].items():
+		if 'dp_stats' in qc_key:
+			if 'min' in qc_key:
+				val = variant_qc.variant_qc.dp_stats.min
+			elif 'max' in qc_key:
+				val = variant_qc.variant_qc.dp_stats.max
+			elif 'mean' in qc_key:
+				val = variant_qc.variant_qc.dp_stats.mean
+			elif 'stdev' in qc_key:
+				val = variant_qc.variant_qc.dp_stats.stdev
+			else:
+				raise ValueError(f'Invalid QC parameter: {qc_key}')
+		elif 'gq_stats' in qc_key:
+			if 'min' in qc_key:
+				val = variant_qc.variant_qc.gq_stats.min
+			elif 'max' in qc_key:
+				val = variant_qc.variant_qc.gq_stats.max
+			elif 'mean' in qc_key:
+				val = variant_qc.variant_qc.gq_stats.mean
+			elif 'stdev' in qc_key:
+				val = variant_qc.variant_qc.gq_stats.stdev
+			else:
+				raise ValueError(f'Invalid QC parameter: {qc_key}')
+		else:
+			val = variant_qc.variant_qc[qc_key]
+
+		if qc_comparison == '>':
+			var_filters.append(val > qc_val)
+		elif qc_comparison == '>=':
+			var_filters.append(val >= qc_val)
+		elif qc_comparison == '<':
+			var_filters.append(val < qc_val)
+		elif qc_comparison == '<=':
+			var_filters.append(val <= qc_val)
+				
+	if len(var_filters) > 0:
+		variant_qc = variant_qc.filter(hl.all(lambda x: x, var_filters))
+
+	# variant_qc = variant_qc.filter(
+	# 	(variant_qc.variant_qc.call_rate >= qc_params['variant']['call_rate']) &
+	# 	(variant_qc.variant_qc.p_value_hwe >= qc_params['variant']['p_value_hwe'])
+	# )
 
 	# Filter data to variants in variant_QC
 	mt = mt.semi_join_rows(variant_qc)
 
 	# Compute sample QC
+	"""
+	'sample_qc': struct {
+        dp_stats: struct {
+            mean: float64, 
+            stdev: float64, 
+            min: float64, 
+            max: float64
+        }, 
+        gq_stats: struct {
+            mean: float64, 
+            stdev: float64, 
+            min: float64, 
+            max: float64
+        }, 
+        call_rate: float64, 
+        n_called: int64, 
+        n_not_called: int64, 
+        n_filtered: int64, 
+        n_hom_ref: int64, 
+        n_het: int64, 
+        n_hom_var: int64, 
+        n_non_ref: int64, 
+        n_singleton: int64, 
+        n_snp: int64, 
+        n_insertion: int64, 
+        n_deletion: int64, 
+        n_transition: int64, 
+        n_transversion: int64, 
+        n_star: int64, 
+        r_ti_tv: float64, 
+        r_het_hom_var: float64, 
+        r_insertion_deletion: float64
+    }
+	"""
 	mt = hl.sample_qc(mt)
 
 	# Apply sample QC filters to sample_QC
-	mt = mt.filter_cols(
-		(mt.sample_qc.call_rate >= qc_params['sample']['call_rate'])
-	)
+	samp_filters = []
+
+	for qc_key, (qc_val, qc_comparison) in qc_params['sample'].items():
+		if 'dp_stats' in qc_key:
+			if 'min' in qc_key:
+				val = mt.sample_qc.dp_stats.min
+			elif 'max' in qc_key:
+				val = mt.sample_qc.dp_stats.max
+			elif 'mean' in qc_key:
+				val = mt.sample_qc.dp_stats.mean
+			elif 'stdev' in qc_key:
+				val = mt.sample_qc.dp_stats.stdev
+			else:
+				raise ValueError(f'Invalid QC parameter: {qc_key}')
+		elif 'gq_stats' in qc_key:
+			if 'min' in qc_key:
+				val = mt.sample_qc.gq_stats.min
+			elif 'max' in qc_key:
+				val = mt.sample_qc.gq_stats.max
+			elif 'mean' in qc_key:
+				val = mt.sample_qc.gq_stats.mean
+			elif 'stdev' in qc_key:
+				val = mt.sample_qc.gq_stats.stdev
+			else:
+				raise ValueError(f'Invalid QC parameter: {qc_key}')
+		else:
+			val = mt.sample_qc[qc_key]
+
+		if qc_comparison == '>':
+			samp_filters.append(val > qc_val)
+		elif qc_comparison == '>=':
+			samp_filters.append(val >= qc_val)
+		elif qc_comparison == '<':
+			samp_filters.append(val < qc_val)
+		elif qc_comparison == '<=':
+			samp_filters.append(val <= qc_val)
+
+	if len(samp_filters) > 0:
+		mt = mt.filter_cols(hl.all(lambda x: x, samp_filters))
+
+	# mt = mt.filter_cols(
+	# 	(mt.sample_qc.call_rate >= qc_params['sample']['call_rate'])
+	# )
 
 	# Drop sample QC data and save as MT
 	mt = mt.drop('sample_qc')
 	mt.write(write_path, overwrite=True)
 	
 	# Save locus/alleles as BGEN
-	if args.write_path_qc_locus_bgen is not None:
+	if args.write_path_gc_locus_tsv is not None:
 		if args.ukb_db_name is not None:
-			write_path_bgen = f'file://{args.write_path_qc_locus_bgen}'
+			write_path_tsv = f'file://{args.write_path_gc_locus_tsv}'
 		else:
-			write_path_bgen = args.write_path_qc_locus_bgen
-		sites_mt = hl.MatrixTable.from_rows_table(mt.rows())
-		sites_mt = sites_mt.drop(
-			*list(sites_mt.row_value.keys())
-		)
-		hl.export_bgen(
-			sites_mt,
-			write_path_bgen,
-			compresssion_codec='zlib'	
-		)
+			write_path_tsv = args.write_path_gc_locus_tsv
+		
+		mt.row_key.export(write_path_tsv)
 
-		# Index BGEN
-		hl.index_bgen(write_path_bgen)
+	"""To read in TSV later:
+
+	t_map = {'locus': hl.tstr, 'alleles': hl.tarray(hl.tstr)}
+	lmt = hl.import_table(
+		write_path_tsv,
+		types=t_map,
+	)
+	# lht = lht.annotate(locus=hl.parse_locus(lht.locus))
+	lht = lht.key_by('locus', 'alleles')
+
+	
+	"""
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
 		'console_scripts': [
 			'bgen-to-mt = gwas_qc.geno_qc.bgen_to_mt:main',
 			'compute-variant-qc = gwas_qc.geno_qc.compute_variant_qc:main',
+			'apply-geno-qc = gwas_qc.geno_qc.apply_geno_qc:main',
 		]
 	}
 )
