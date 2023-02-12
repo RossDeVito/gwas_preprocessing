50edc16cf3adfdca21317d732b66dc5a55eee140
Ross DeVito <ross@Rosss-MBP.lan>
2023-02-12 13:06:16 -0800

    fix rel path

--- scripts/ukb/ukb_subset_vep_anno.sh
+++ scripts/ukb/ukb_subset_vep_anno.sh
@@ -2,6 +2,6 @@
 
 vep-anno \
 	-v /mnt/project/rdevito/project1_data/subset/subset_rare_loci.tsv \
-	-c ../../scripts/vep_specs/vep_ukb_rap.json \
+	-c /../../scripts/vep_specs/vep_ukb_rap.json \
 	-o subset_vep_anno.ht \
 	-u rdevito_p1_db
