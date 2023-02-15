874d07148c158d9bd6a0fad2c14ed7d38992e47a
Ross DeVito <ross@Rosss-MBP.lan>
2023-02-14 19:12:15 -0800

    fix path

--- scripts/ukb/ukb_19_agg_anno_v0.sh
+++ scripts/ukb/ukb_19_agg_anno_v0.sh
@@ -3,6 +3,6 @@
 agg-anno \
 	-m chr19_qced.mt \
 	-a chr19_vep_anno.ht \
-	-j ../../scripts/vep_agg_json/common_lof_by_gene_id_oms.json \
+	-j /opt/notebooks/gwas_preprocessing/scripts/vep_agg_json/common_lof_by_gene_id_oms.json \
 	-s /home/dnanexus/chr19_common_lof_by_gene_id_oms.parquet \
 	-u rdevito_p1_db
