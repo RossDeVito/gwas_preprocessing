7e4cb019ebb850eec944e5938412ab205519f057
Ross DeVito <ross@Rosss-MacBook-Pro.local>
2023-02-21 11:29:48 -0800

    update rare and common scripts

--- scripts/ukb/ukb_19_agg_anno_v0_common.sh
+++ scripts/ukb/ukb_19_agg_anno_v0_common.sh
@@ -3,7 +3,7 @@
 agg-anno \
 	-m chr19_qced.mt \
 	-a chr19_vep_anno.ht \
-	-j /home/dnanexus/gwas_preprocessing/scripts/vep_agg_json/common_lof_by_gene_id_oms.json \
+	-j /home/dnanexus/gwas_preprocessing/scripts/vep_agg_json/v0_common_lof_by_gene_id_oms.json \
 	-s /home/dnanexus/chr19_common_lof_by_gene_id_oms.parquet \
 	-u rdevito_p1_db
 
---
+++ scripts/ukb/ukb_19_agg_anno_v0_rare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+agg-anno \
+	-m chr19_qced.mt \
+	-a chr19_vep_anno.ht \
+	-j /home/dnanexus/gwas_preprocessing/scripts/vep_agg_json/v0_rare_lof_by_gene_id_oms.json \
+	-s /home/dnanexus/chr19_rare_lof_by_gene_id_oms.parquet \
+	-u rdevito_p1_db
+
+dx upload chr19_rare_lof_by_gene_id_oms.parquet \
+	--path "project-GG25fB8Jv7B928vqK7k6vYY6:/rdevito/project1_data/chr19/agg_tables/"
