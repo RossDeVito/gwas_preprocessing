e69d0046eef446fbe378d61a349326bcbd6c06ff
Ross DeVito <ross@Rosss-MacBook-Pro.local>
2023-02-21 10:52:23 -0800

    add fixed dx upload to script

--- scripts/ukb/ukb_19_agg_anno_v0.sh
+++ scripts/ukb/ukb_19_agg_anno_v0.sh
@@ -6,3 +6,6 @@ agg-anno \
 	-j /home/dnanexus/gwas_preprocessing/scripts/vep_agg_json/common_lof_by_gene_id_oms.json \
 	-s /home/dnanexus/chr19_common_lof_by_gene_id_oms.parquet \
 	-u rdevito_p1_db
+
+dx upload chr19_common_lof_by_gene_id_oms.parquet \
+	--path "project-GG25fB8Jv7B928vqK7k6vYY6:/rdevito/project1_data/chr19/agg_tables/"
