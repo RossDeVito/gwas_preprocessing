e3dd2846409e752b603c9e48b890e78e04116b28
Ross DeVito <ross@Rosss-MBP.lan>
2023-03-02 13:59:29 -0800

    change install of tools

--- scripts/ukb/ukb_19_rewite_test.sh
+++ scripts/ukb/ukb_19_rewite_test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rewrite-agg-table \
+# Run w/o installing gwas_qc
+
+python rewrite_agg_table.py \
 	-p '/mnt/project/rdevito/project1_data/chr19/agg_tables/chr19_common_lof_by_gene_id_oms.parquet/'
 	-s /home/dnanexus/rr_chr19_common_lof_by_gene_id_oms.parquet
 	-c 2
