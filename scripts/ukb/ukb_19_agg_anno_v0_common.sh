d89a8591df0cba52088c8f11b8183db5836e7f93
Ross DeVito <ross@Rosss-MBP.lan>
2023-03-02 14:27:57 -0800

    add rewrite to scripts

--- scripts/ukb/ukb_19_agg_anno_v0_common.sh
+++ scripts/ukb/ukb_19_agg_anno_v0_common.sh
@@ -4,8 +4,13 @@ agg-anno \
 	-m chr19_qced.mt \
 	-a chr19_vep_anno.ht \
 	-j /home/dnanexus/gwas_preprocessing/scripts/vep_agg_json/v0_common_lof_by_gene_id_oms.json \
-	-s /home/dnanexus/chr19_common_lof_by_gene_id_oms.parquet \
+	-s /home/dnanexus/unindexed_chr19_common_lof_by_gene_id_oms.parquet \
 	-u rdevito_p1_db
+
+python ../../annotation/rewrite_agg_table.py \
+	-p /home/dnanexus/unindexed_chr19_common_lof_by_gene_id_oms.parquet \
+	-s /home/dnanexus/chr19_common_lof_by_gene_id_oms.parquet \
+	-c 2
 
 dx upload -r /home/dnanexus/chr19_common_lof_by_gene_id_oms.parquet \
 	--path "project-GG25fB8Jv7B928vqK7k6vYY6:/rdevito/project1_data/chr19/agg_tables/"
--- scripts/ukb/ukb_19_rewite_test.sh
+++ scripts/ukb/ukb_19_rewite_test.sh
@@ -2,7 +2,7 @@
 
 # Run w/o installing gwas_qc
 
-ipython -i -- ../../annotation/rewrite_agg_table.py \
+python ../../annotation/rewrite_agg_table.py \
 	-p '/mnt/project/rdevito/project1_data/chr19/agg_tables/chr19_common_lof_by_gene_id_oms.parquet/' \
 	-s /home/dnanexus/rr_chr19_common_lof_by_gene_id_oms.parquet \
 	-c 2
